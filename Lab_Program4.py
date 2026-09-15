9b3a037957de0cbf16c49b92461fcc6738ef3822
KDakshayani <karimaddeladakshayani@gmail.com>
2026-09-15 12:30:24 +0530

    Update Lab_Program4.py

--- Lab_Program4.py
+++ Lab_Program4.py
@@ -4,14 +4,7 @@ b= float(input("Enter the second number: "))
 
 add = a + b
 sub= a - b
-mul=a*b
+
 
 print(f"The sum of {a} and {b} is: {add}")
 print(f"The difference between {a} and {b} is: {sub}")
-print(f"The product of {a} and {b} is: {mul}")
-
-if b!=0:
-    div=a/b
-    print(f"The quotient of {a} and {b} is: {div}")
-else:
-    print("Can't divided by zero")
